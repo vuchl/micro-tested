f052c2ef0509695e497fb2cbe317f4633c91fba8
vuchl <vuchl@gmx.de>
2017-01-27 06:24:37 +0100

    add some comments to tests and updated  to work without the  dependency

--- test/simpleEndpointSpec.js
+++ test/simpleEndpointSpec.js
@@ -2,17 +2,20 @@
 const expect = require('chai').expect
 
 // get the url of a micro served microService
-const listen = require('test-listen').default
+const listen = require('test-listen')
 
 // http library
 const request = require('request')
 const micro = require('micro')
 
 describe(`the endpoint facing the web`, async () => {
+  // setup simple micro service
   const microService = require(process.cwd() + '/src/index.js')
   const server = micro(microService)
-
+  
+  // spin the service up and fetch the url (since the port is dynamic)
   const url =  await listen(server)
+
   describe('GET /', () => {
     it(`returns status code 200`, (done) => {
       request.get(url, (error, response, body) => {
